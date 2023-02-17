3b83494af6f746ced595dcce561a3334ebefff01
swraj28 <swarajkant@gmail.com>
2023-02-17 11:57:09 +0530

    Removal of Items from Cart

--- controller/cart_controller.js
+++ controller/cart_controller.js
@@ -69,3 +69,34 @@ exports.add_to_cart = async (req, res) => {
     res.status(404).json({ success: false, message: "Something went worng!", data: err });
   }
 };
+
+exports.remove_from_cart = async (req, res) => {
+  try{
+    const _id=(req.params.id);
+    const cart = await cart_model.findOne({_id});
+    if(cart){
+      const data = {
+        id: req.body.id,
+      };
+      // Use findOneAndUpdate() to find the cart document with the given _id and update it
+      const updatedCart = await cart_model.findOneAndUpdate(
+        { _id: cart }, // Filter criteria to find the cart document to update
+        { $pull: { products: { _id:data.id } } }, // Update operation to remove item from products array
+        { new: true } // Options to return the updated document instead of the original document
+      );
+      await updatedCart.save();
+      return res.status(200).json({
+        success: true,
+        message: "Successfully removed from cart",
+        data: updatedCart
+      })
+    }else{
+      return res.status(404).json({
+        success: false,
+        message: "Cart not found"
+      })
+    }
+  }catch(err){
+    res.status(404).json({ success: false, message: "Something went worng!", data: err });
+  }
+}
--- routes/cart.js
+++ routes/cart.js
@@ -6,5 +6,6 @@ const controller = require("../controller/cart_controller");
 router.post("/create",controller.create_cart);
 router.get("/:id/get_cart",controller.get_cart);
 router.post("/:id/add_cart",controller.add_to_cart);
+router.post("/:id/remove_cart",controller.remove_from_cart);
 
 module.exports= router;

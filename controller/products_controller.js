af3d20fb631d547d9bc30a2a12b6c7c916f314ec
swraj28 <swarajkant@gmail.com>
2023-02-27 17:10:54 +0530

    Adding prop of products_controller

--- controller/products_controller.js
+++ controller/products_controller.js
@@ -2,7 +2,7 @@ const products_model= require('../model/products_model')
 const category_model= require('../model/category_model')
 const sub_category_model= require('../model/sub_category_model')
 const flash_sale_model= require('../model/flash_sale_model');
-
+const cart_model= require('../model/cart_model');
 
 exports.get_all_products= async(req,res)=>{
   try{
@@ -86,6 +86,60 @@ exports.createOneProduct = async (req, res) => {
       data: response
     });
   } catch (error) {
-    res.status(404).json({ success: false, message: "Something went worng !", data: error.errors });
+    res.status(404).json({ success: false, message: "Something went worng !", data: error });
+  }
+};
+
+exports.updateOneProduct = async (req, res) => {
+  try{
+    const _id = (req.params.id);
+    await products_model.updateOne({ _id },{$set:req.body});
+
+    const product = await products_model.findOne({_id});
+
+    res.status(200).send({
+      success: true,
+      message: "Product updated successfully",
+      data: product
+    });
+  }catch(err){
+    res.status(404).json({ success: false, message: "Something went worng !", data: err});
+  }
+}
+
+exports.delete_product = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const product = await products_model.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ success: false, message: "Product not found" });
+    }
+
+    // Delete the product from the cart
+
+    const carts = await cart_model.find({ "products.productId": productId });
+
+    for (let cart of carts) {
+      const productIndex = cart.products.findIndex((product) => product.productId == productId);
+      if (productIndex !== -1) {
+        cart.products.splice(productIndex, 1); // Remove the product from the cart
+
+        await cart.save(); // Save the updated cart
+      }
+    }
+
+    // Delete the product from the subcategory and category
+
+
+    // Delete the product from the flashsale
+    
+
+    // Delete the product
+    await product.remove();
+
+    return res.status(200).json({ success: true, message: "Product deleted successfully", data: product });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: "Internal server error", data: error });
   }
 };
--- routes/products_routes.js
+++ routes/products_routes.js
@@ -6,5 +6,7 @@ const controller= require("../controller/products_controller")
 router.post('/create',controller.createOneProduct);
 router.get('/get-all',controller.get_all_products);
 router.get('/get-one/:id',controller.getOneProduct);
+router.post('/update-one/:id',controller.updateOneProduct);
+router.delete('/delete-one/:id',controller.delete_product);
 
 module.exports= router;
